e953403c5cb01d53c4d6e6b3e9408d6e5cb33722
Karim648 <151876709+Karim648@users.noreply.github.com>
2025-09-11 10:16:57 -0400

    feat: add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
-    <>
-      <SignInButton />
-      <UserButton />
-    </>
+    <div className="space-y-4 p-4">
+      <div className="flex items-center gap-4">
+        <SignInButton />
+        <UserButton />
+        <ThemeToggle />
+      </div>
+    </div>
   );
 }
