e9866417da6eaed3e3fdbd887ead7a6df48a151d
Karim648 <151876709+Karim648@users.noreply.github.com>
2025-09-12 09:14:43 -0400

    feat: add navbar

---
+++ src/app/app/_Navbar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { UserAvatar } from "@/components/UserAvatar";
+import { SignOutButton, useClerk } from "@clerk/nextjs";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  BookOpenIcon,
+  BrainCircuitIcon,
+  FileSlidersIcon,
+  LogOut,
+  SpeechIcon,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+
+const navLinks = [
+  { name: "Interviews", href: "interviews", Icon: SpeechIcon },
+  { name: "Questions", href: "questions", Icon: BookOpenIcon },
+  { name: "Resume", href: "resume", Icon: FileSlidersIcon },
+];
+
+export function Navbar({ user }: { user: { name: string; imageUrl: string } }) {
+  const { openUserProfile } = useClerk();
+
+  return (
+    <nav className="h-header border-b">
+      <div className="container flex h-full items-center justify-between">
+        <Link href="/app" className="flex items-center gap-2">
+          <BrainCircuitIcon className="text-primary size-8" />
+          <span className="text-xl font-bold">Resumate AI</span>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          {navLinks.map((link) => (
+            <Button key={link.name}>
+              <Link href={link.href} className="flex items-center gap-2">
+                <link.Icon />
+                {link.name}
+              </Link>
+            </Button>
+          ))}
+
+          <ThemeToggle />
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <UserAvatar user={user} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onClick={() => openUserProfile()}>
+                <User className="mr-2" />
+                Profile
+              </DropdownMenuItem>
+              <SignOutButton>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2" />
+                  Logout
+                </DropdownMenuItem>
+              </SignOutButton>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,66 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { db } from "@/db";
+import { JobInfoTable } from "@/db/schema";
+import { getJobInfoUserTag } from "@/features/jobInfos/dbCache";
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { desc, eq } from "drizzle-orm";
+import { Loader2Icon } from "lucide-react";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { Suspense } from "react";
+
 export default function AppPage() {
-  return <h1>Welcome to app page</h1>;
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen-header flex items-center justify-center">
+          <Loader2Icon className="size-24 animate-spin" />
+        </div>
+      }
+    >
+      <JobInfos />
+    </Suspense>
+  );
+}
+
+async function JobInfos() {
+  const { userId, redirectToSignIn } = await getCurrentUser({});
+
+  if (userId == null) return redirectToSignIn();
+
+  const jobInfos = await getJobInfos(userId);
+
+  if (jobInfos.length === 0) {
+    return <NoJobInfos />;
+  }
+}
+
+function NoJobInfos() {
+  return (
+    <div className="container my-4 max-w-5xl">
+      <h1 className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+        Welcome to Resumate AI
+      </h1>
+      <p className="text-muted-foreground mb-8">
+        To get started, enter information about the type of job you are wanting
+        to apply for. This can be specific information copied directly from a
+        job listing or general information such as the tech stack you want to
+        work in. The more specific you are in the description the closer the
+        test interviews will be to the real thing.
+      </p>
+      <Card>
+        <CardContent>{/* <JobInfoForm /> */}</CardContent>
+      </Card>
+    </div>
+  );
+}
+
+async function getJobInfos(userId: string) {
+  "use cache";
+
+  cacheTag(getJobInfoUserTag(userId));
+
+  return await db.query.JobInfoTable.findMany({
+    where: eq(JobInfoTable.userId, userId),
+    orderBy: desc(JobInfoTable.updatedAt),
+  });
 }
